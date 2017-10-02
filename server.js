dc5c2f0f2aef6561bda18bfc849ceb021931ef1e
Angie <anp58@cornell.edu>
2017-10-02 00:39:13 -0400

    json sort

--- server.js
+++ server.js
@@ -38,11 +38,10 @@ router.get('/season/:season/format/:format', function(req, res, next) {
                         rp(domain + urls[i])
                             .then(function(htmlString) {
                                     quote_data.push(office_quote_extractor.quotes(htmlString));
-                                    quote_data.sort((a,b) => parseInt(a.episode) - parseInt(b.episode))
                                     if (quote_data.length == urls.length) {
                                             // quote_data = {"episode" : q_data.reduce(function (acc, curr) { acc[curr[1]] = {"name": curr[2], "quotes": curr[0]} ; return acc; }, {} )};
                                             if (req.params.format == "quotes") {
-                                                res.json({data: quote_data});
+                                                res.json({data: quote_data.sort((a,b) => parseInt(a.episode) - parseInt(b.episode))});
                                             }
                                             else if (req.params.format == "connections") {
                                                 res.locals = {data: quote_data};
